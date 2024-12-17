0d6009a24eb4f9da33d41162ee85f88c533ab011
Yusei Kajihara <j3323ajg@sit.ac.jp>
2024-12-17 21:08:21 +0900

    tuika

--- train.py
+++ train.py
@@ -73,6 +73,44 @@ for k in range(n_epochs):
 
     acc_train = models.test_accuracy(model,dataloader_test,loss_fn)
     print(f'train accuracy :{acc_train*100:.3f}%')
-    acc_test = models.test(model,dataloader_test)
+    acc_test = models.test_accuracy(model,dataloader_test)
     print(f'test accuracy:{acc_test*100:.3f}%')
     
+loss_train_history = [] 
+loss_test_history = []
+acc_train_history = [] 
+acc_test_history = [] 
+
+for k in range(n_epochs): 
+    print(f'epoch {k+1}/{n_epochs}', end=': ', flush=True)#1 epoch の学習 
+
+    loss_train = models.train(model, dataloader_train, loss_fn) 
+    loss_train_history.append(loss_train) 
+    print(f'train loss: {loss_train: 3f}', end=', ') 
+    
+    loss_test = models.test(model, dataloader_test,loss_fn) 
+    loss_test_history.append(loss_test) 
+    print(f'test loss: {loss_test: 3f}', end=', ') 
+    
+    acc_train = models.test_accuracy(model, dataloader_train)
+    acc_train_history.append(acc_train) 
+    print(f'train accuracy: {acc_train*100:.3f}%',end=', ')
+    
+    acc_test = models.test_accuracy(model,dataloader_test)
+    acc_test_history.append(acc_test)
+    print(f'test accuracy: {acc_test*100:.3f}%')
+
+    plt.plot(acc_train_history, label='train') 
+    plt.plot(acc_test_history, label='test') 
+    plt.xlabel('epochs') 
+    plt.ylabel('accuracy') 
+    plt.legend() 
+    plt.grid() 
+    plt.show() 
+    plt.plot(loss_train_history, label='train') 
+    plt.plot(loss_test_history, label='test') 
+    plt.xlabel('epochs') 
+    plt.ylabel('loss') 
+    plt.legend() 
+    plt.grid() .show
+
