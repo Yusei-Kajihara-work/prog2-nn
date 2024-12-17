bf3d842710a2f5e3345e61fede52e6194c285484
Yusei Kajihara <j3323ajg@sit.ac.jp>
2024-12-17 16:14:57 +0900

    pridict をコミット

---
+++ predict.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+import torch
+from torchvision import datasets
+import torchvision.transforms.v2 as transforms
+
+import models
+
+model = models.MyModel()
+print(model)
